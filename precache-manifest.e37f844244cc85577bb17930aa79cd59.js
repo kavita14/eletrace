a01fc3ee32167dc4adc26b1b90d1ebb61bf3685e
Kavita Sharma <kavitas@lsnsoft.com>
2021-09-14 16:33:05 +0530

    Updates

---
+++ precache-manifest.e37f844244cc85577bb17930aa79cd59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8359424c65e1b449f5015087e26a690",
+    "url": "/eletrace/index.html"
+  },
+  {
+    "revision": "aa0bdd38a0aaa3109e34",
+    "url": "/eletrace/static/css/main.1c01dc44.chunk.css"
+  },
+  {
+    "revision": "338bff7a19405f884d6c",
+    "url": "/eletrace/static/js/2.8a3691c9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/eletrace/static/js/2.8a3691c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa0bdd38a0aaa3109e34",
+    "url": "/eletrace/static/js/main.998e322a.chunk.js"
+  },
+  {
+    "revision": "148d499f264ff86c765f",
+    "url": "/eletrace/static/js/runtime-main.d44bed34.js"
+  },
+  {
+    "revision": "7e097849282b49fa8a77dc6067576c46",
+    "url": "/eletrace/static/media/box-image1.7e097849.png"
+  }
+]);
